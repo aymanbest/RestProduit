f8137c950249be62c063bfb271331219a97083b7
Ayman Faik <legmapapa@gmail.com>
2024-02-26 10:15:14 +0100

    a delete endpoint

--- productRoutes.js
+++ productRoutes.js
@@ -22,9 +22,27 @@ router.get('/:id', async (req, res) => {
         const product = await collection.findOne({ _id: new ObjectId(req.params.id) })
         res.json(product)
     } catch(e) {
-        console.log(e.message);
+        res.json(e.message);
     }
 })
+
+
+router.delete('/:id', async (req, res) => {
+    const db = req.app.locals.db;
+    const collection = db.collection('produits');
+    const id = parseInt(req.params.id);
+    try {
+        const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+        if (result.deletedCount === 1) {
+            res.status(200).send('produit deleted successfully');
+        } else {
+            res.status(404).send('produit not found');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error deleting produit');
+    }
+});
 
 
 module.exports = router
