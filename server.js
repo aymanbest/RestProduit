8fa4f8c41caccd4d9b12012704d3a24befc30f9a
Ayman Faik <legmapapa@gmail.com>
2024-02-26 10:30:40 +0100

    some patching on the server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const productRoutes = require('./productRoutes'); // Importer le routeur d'authentification
 const { MongoClient } = require('mongodb');
 const { verify } = require('crypto');
+const Auth = require('./auth').Auth;
+const verifyToken = require('./auth').verifyToken;
 
 const app = express();
 const url = 'mongodb://127.0.0.1:27017';
@@ -11,7 +13,9 @@ let db;
 // Middleware pour le parsing du corps de la requête
 app.use(express.json());
 
-app.use('/produit', productRoutes);
+app.use('/produit', productRoutes , Auth);
+//app.use('/produit', Auth);
+//app.use(verifyToken);
 // Middleware pour la connexion à la base de données MongoDB
 MongoClient.connect(url)
     .then((client) => {
